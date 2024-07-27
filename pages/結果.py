ab85499268ffb9501b199565e766a54ec6e9616d
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2024-07-27 16:06:58 +0900

    configを最初に

--- pages/結果.py
+++ pages/結果.py
@@ -1,13 +1,13 @@
 import streamlit as st
+st.set_page_config(
+    page_title="GBBdata-lab"
+)
 st.markdown("""
     <script>
         window.location.replace("https://gbbinfo-jpn.onrender.com/");
     </script>
 """, unsafe_allow_html=True)
 
-st.set_page_config(
-    page_title="GBBdata-lab"
-)
 
 st.title('GBBdata-lab')
 st.write("当ページはサービスを終了しました。今後は[GBBINFO-JPN](https://gbbinfo-jpn.onrender.com/)をご利用ください。")
